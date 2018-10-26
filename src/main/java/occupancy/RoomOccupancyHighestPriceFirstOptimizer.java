6eb9369e7f604de9abf49cbf5f82a60276863c7c
Jonathan Morales Vélez <jonathanmv@outlook.com>
2018-10-26 13:16:04 +0200

    Implementation of the Highest price first optimizer

--- src/main/java/occupancy/RoomOccupancyHighestPriceFirstOptimizer.java
+++ src/main/java/occupancy/RoomOccupancyHighestPriceFirstOptimizer.java
@@ -1,5 +1,10 @@
 package occupancy;
 
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.DoubleStream;
+import java.util.stream.Stream;
+
 public class RoomOccupancyHighestPriceFirstOptimizer implements IRoomOccupancyOptimizer {
     private double upperLimit;
     private double lowerLimit;
@@ -37,6 +42,32 @@ public class RoomOccupancyHighestPriceFirstOptimizer implements IRoomOccupancyOp
 
     @Override
     public RoomsOccupationState optimizeRoomOccupation(RoomsOccupationState roomsOccupationState) {
-        return null;
+        List<Double> potentialGuests = roomsOccupationState.getPotentialGuests();
+        if (potentialGuests.size() < 1) {
+            return roomsOccupationState;
+        }
+        potentialGuests.sort(Collections.reverseOrder());
+
+        RoomAvailabilityState roomAvailability = roomsOccupationState.getRoomAvailabilityStateByType(roomType);
+        int availability = roomAvailability.getAvailability();
+
+        List<Double> deniedGuests = potentialGuests.stream()
+                .filter(price -> upperLimit <= price || price <= lowerLimit)
+                .collect(Collectors.toList());
+        List<Double> availableGuests = potentialGuests.stream()
+                .filter(price -> upperLimit > price && price > lowerLimit)
+                .collect(Collectors.toList());
+
+        int assignedGuests = roomAvailability.assignGuests(availableGuests);
+
+        List<Double> unassignedGuests = availableGuests.stream()
+                .skip(assignedGuests)
+                .collect(Collectors.toList());
+
+        List<Double> newPotentialGuests = Stream.concat(deniedGuests.stream(), unassignedGuests.stream())
+                .collect(Collectors.toList());
+
+        roomsOccupationState.setPotentialGuests(newPotentialGuests);
+        return roomsOccupationState;
     }
 }
--- src/test/java/occupancy/RoomOccupancyHighestPriceFirstOptimizerTest.java
+++ src/test/java/occupancy/RoomOccupancyHighestPriceFirstOptimizerTest.java
@@ -2,6 +2,9 @@ package occupancy;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class RoomOccupancyHighestPriceFirstOptimizerTest {
@@ -9,16 +12,17 @@ class RoomOccupancyHighestPriceFirstOptimizerTest {
     @Test
     void optimizeRoomOccupation() {
         RoomAvailabilityState economyRooms = new RoomAvailabilityState(RoomType.ECONOMY, 2);
-        RoomAvailabilityState[] availableRooms = { economyRooms };
-        double[] potentialGuests = { 40.0, 120.0, 60.0 };
+        List<RoomAvailabilityState> availableRooms = Arrays.asList(economyRooms);
+
+        List<Double> potentialGuests = Arrays.asList(40.0, 120.0, 60.0);
         RoomsOccupationState roomsOccupationState = new RoomsOccupationState(availableRooms, potentialGuests);
 
         RoomOccupancyHighestPriceFirstOptimizer optimizer = new RoomOccupancyHighestPriceFirstOptimizer(100, 0, RoomType.ECONOMY);
 
         RoomsOccupationState actual = optimizer.optimizeRoomOccupation(roomsOccupationState);
         assertNotNull(actual);
-        assertEquals(1, actual.getRoomTypeRevenue().length);
-        RoomTypeRevenue roomTypeRevenue = actual.getRoomTypeRevenue()[0];
-        assertEquals(100, roomTypeRevenue.getRevenue());
+        assertEquals(1, actual.getRoomTypeRevenueMap().size());
+        Double economyRoomRevenue = actual.getRoomTypeRevenueMap().get(RoomType.ECONOMY);
+        assertEquals(100.0, economyRoomRevenue.doubleValue());
     }
 }
